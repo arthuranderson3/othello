11c370f19bd8d2fea15a4d8b12b02081e7cb440f
Arthur Anderson <arthur.anderson3@gmail.com>
2018-09-11 10:26:45 -0400

    ref atomic testing

--- src/othello/controller/boardTransform/toIdx.test.js
+++ src/othello/controller/boardTransform/toIdx.test.js
@@ -1,16 +1,32 @@
 import toIdx from './toIdx';
 
 describe('toIdx Test Suite', () => {
-  it('#toIdx -- Idx for Row and Col', () => {
+  it('toIdx( {row:0, col:0 })=0', () => {
     expect(toIdx({ row: 0, col: 0 })).toBe(0);
+  });
+  it('toIdx( {row:0, col:1 })=1', () => {
     expect(toIdx({ row: 0, col: 1 })).toBe(1);
+  });
+  it('toIdx( {row:1, col:0 })=8', () => {
     expect(toIdx({ row: 1, col: 0 })).toBe(8);
+  });
+  it('toIdx( {row:1, col:1 })=9', () => {
     expect(toIdx({ row: 1, col: 1 })).toBe(9);
+  });
+  it('toIdx( {row:1, col:2 })=10', () => {
     expect(toIdx({ row: 1, col: 2 })).toBe(10);
   });
 
-  it('#toIdx -- out of bounds for toIdx', () => {
+  it('toIdx( { row: 0, col: undefined }).undefined', () => {
     expect(toIdx({ row: 0, col: undefined })).toBeUndefined();
+  });
+  it('toIdx( { row: undefined, col: 0 }).undefined', () => {
     expect(toIdx({ row: undefined, col: 0 })).toBeUndefined();
+  });
+  it('toIdx( { row: 10, col: 0 }).undefined', () => {
+    expect(toIdx({ row: 10, col: 0 })).toBeUndefined();
+  });
+  it('toIdx( { row: 0, col: 10 }).undefined', () => {
+    expect(toIdx({ row: 0, col: 10 })).toBeUndefined();
   });
 });
