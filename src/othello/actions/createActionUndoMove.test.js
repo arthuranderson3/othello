8d474bfa17a0ed252813e017841be5db066a1499
Arthur Anderson <arthur.anderson3@gmail.com>
2018-09-11 10:05:47 -0400

    ref test more fine grained

--- src/othello/actions/createActionUndoMove.test.js
+++ src/othello/actions/createActionUndoMove.test.js
@@ -2,9 +2,21 @@ import createActionUndoMove from './createActionUndoMove';
 import { UNDO_MOVE } from './othelloActionConstants';
 
 describe('createActionUndoMove test suite', () => {
-	it('default', () => {
-		const { type, payload } = createActionUndoMove();
-		expect( type ).toEqual(UNDO_MOVE);
-		expect( payload ).toEqual( {} );
+	let action;
+	beforeAll( () => {
+		action = createActionUndoMove();
 	});
+	it( 'action.type', () => {
+		expect( action ).toHaveProperty( 'type');
+	});
+	it( 'action.payload', () => {
+		expect( action ).toHaveProperty( 'payload' );
+	});
+	it( 'action.type=UNDO_MOVE', () => {
+		expect( action.type ).toEqual('UNDO_MOVE');
+	});
+	it('action.payload={}', () => {
+		expect( action.payload ).toEqual( {} );
+	});
+
 });
--- src/othello/controller/boardNavigation/index.test.js
+++ src/othello/controller/boardNavigation/index.test.js
@@ -3,82 +3,114 @@ import { right, left } from './index';
 import { bottom, bottomRight, bottomLeft } from './index';
 
 describe('boardNavigation Test Suite', () => {
-  it('#top - success', () => {
+  it('#top(8)=0', () => {
     expect(top(8)).toBe(0);
+  });
+  it('#top(63)=55', () => {
     expect(top(63)).toBe(55);
   });
 
-  it('#top - out of bounds', () => {
+  it('#top(0).undefined', () => {
     expect(top(0)).toBeUndefined();
+  });
+  it('#top(65).undefined', () => {
     expect(top(65)).toBeUndefined();
   });
 
-  it('#topLeft - success', () => {
+  it('#topLeft(9)=0', () => {
     expect(topLeft(9)).toBe(0);
+  });
+  it('#topLeft(10)=1', () => {
     expect(topLeft(10)).toBe(1);
   });
 
-  it('#topLeft - out of bounds', () => {
+  it('#topLeft(0).undefined', () => {
     expect(topLeft(0)).toBeUndefined();
+  });
+  it('#topLeft(7).undefined', () => {
     expect(topLeft(7)).toBeUndefined();
   });
-  it('#topRight - success', () => {
+  it('#topRight(9)=2', () => {
     expect(topRight(9)).toBe(2);
+  });
+  it('#topRight(10)=3', () => {
     expect(topRight(10)).toBe(3);
   });
 
-  it('#topRight - out of bounds', () => {
+  it('#topRight(0).undefined', () => {
     expect(topRight(0)).toBeUndefined();
+  });
+  it('#topRight(6).undefined', () => {
     expect(topRight(6)).toBeUndefined();
   });
 
-  it('#left - success', () => {
+  it('#left(9)=8', () => {
     expect(left(9)).toBe(8);
+  });
+  it('#left(10)=9', () => {
     expect(left(10)).toBe(9);
   });
 
-  it('#left - out of bounds', () => {
+  it('#left(0).undefined', () => {
     expect(left(0)).toBeUndefined();
+  });
+  it('#left(8).undefined', () => {
     expect(left(8)).toBeUndefined();
   });
 
-  it('#right - success', () => {
+  it('#right(9)=10', () => {
     expect(right(9)).toBe(10);
+  });
+  it('#right(10)=11', () => {
     expect(right(10)).toBe(11);
   });
 
-  it('#right - out of bounds', () => {
+  it('#right(7).undefined', () => {
     expect(right(7)).toBeUndefined();
+  });
+  it('#right(63).undefined', () => {
     expect(right(63)).toBeUndefined();
   });
 
-  it('#bottom - success', () => {
+  it('#bottom(0)=8', () => {
     expect(bottom(0)).toBe(8);
+  });
+  it('#bottom(1)=9', () => {
     expect(bottom(1)).toBe(9);
   });
 
-  it('#bottom - out of bounds', () => {
+  it('#bottom(-1).undefined', () => {
     expect(bottom(-1)).toBeUndefined();
+  });
+  it('#bottom(58).undefined', () => {
     expect(bottom(58)).toBeUndefined();
   });
 
-  it('#bottomRight - success', () => {
+  it('#bottomRight(0)=9', () => {
     expect(bottomRight(0)).toBe(9);
+  });
+  it('#bottomRight(1)=10', () => {
     expect(bottomRight(1)).toBe(10);
   });
 
-  it('#bottomRight - out of bounds', () => {
+  it('#bottomRight(7).undefined', () => {
     expect(bottomRight(7)).toBeUndefined();
+  });
+  it('#bottomRight(63).undefined', () => {
     expect(bottomRight(63)).toBeUndefined();
   });
 
-  it('#bottomLeft - success', () => {
+  it('#bottomLeft(1)=8', () => {
     expect(bottomLeft(1)).toBe(8);
+  });
+  it('#bottomLeft(2)=9', () => {
     expect(bottomLeft(2)).toBe(9);
   });
 
-  it('#bottomLeft - out of bounds', () => {
+  it('#bottomLeft(0).undefined', () => {
     expect(bottomLeft(0)).toBeUndefined();
+  });
+  it('#bottomLeft(62).undefined', () => {
     expect(bottomLeft(62)).toBeUndefined();
   });
 });
