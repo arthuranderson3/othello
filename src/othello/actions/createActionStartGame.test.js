e58012a9dabf1a1a630ea0339d2601f6d4cfa089
Arthur Anderson <arthur.anderson3@gmail.com>
2018-09-11 09:44:30 -0400

    ref test more fine grained

--- src/othello/actions/createActionStartGame.test.js
+++ src/othello/actions/createActionStartGame.test.js
@@ -2,18 +2,39 @@ import createActionStartGame from './createActionStartGame';
 import { START_GAME } from './othelloActionConstants';
 
 describe('createActionStartGame', () => {
-	it('default', () => {
-		const { type, payload } = createActionStartGame();
-		expect( type ).toEqual( START_GAME );
-		expect( payload ).toHaveProperty( 'gameName' );
-		expect( payload ).toHaveProperty( 'playerName');
-		expect( payload.gameName ).toEqual('anonymous');
+	let startGame;
+	let payload;
+	beforeAll( () => {
+		startGame = createActionStartGame();
+		payload = startGame.payload;
+	});
+	it('startGame.type', () => {
+		expect(startGame).toHaveProperty('type');
+	});
+	it('startGame.type=START_GAME', () => {
+		expect(startGame.type).toEqual(START_GAME);
+	});
+	it('startGame.payload', () => {
+		expect( startGame ).toHaveProperty('payload');
+	});
+	it('startGame.payload.gameName', () => {
+		expect( payload ).toHaveProperty('gameName')
+	});
+	it('startGame.payload.gameName=anonymous', () => {
+		expect(payload.gameName).toEqual('anonymous');
+	});
+	it('startGame.payload.playerName', () => {
+		expect( payload ).toHaveProperty('playerName');
+	});
+	it('startGame.payload.playerName', () => {
 		expect( payload.playerName ).toEqual('white');
 	});
-	it('w/params', () => {
+	it('w/params payload.gameName=test_game_name', () => {
 		const { type, payload } = createActionStartGame( 'test_game_name', 'player_one');
-		expect( type ).toEqual( START_GAME );
 		expect( payload.gameName ).toEqual( 'test_game_name' );
-		expect( payload.playerName ).toEqual('player_one');
 	});
+	it('w/params payload.playerName=player_one', () => {
+		const { type, payload } = createActionStartGame( 'test_game_name', 'player_one');
+		expect( payload.playerName ).toEqual('player_one');
+	})
 });
