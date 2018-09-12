cffcf02c9d0177f15a3c90e01dfa0cc0e4696227
Arthur Anderson <arthur.anderson3@gmail.com>
2018-09-12 10:00:18 -0400

    ref atomic tests

--- src/othello/controller/moveLogic/accumulateIdxForMove.test.js
+++ src/othello/controller/moveLogic/accumulateIdxForMove.test.js
@@ -4,11 +4,20 @@ import createGameBoardPieces from '../../model/gameBoardPieces/createGameBoardPi
 import toSquaresArray from '../../model/gameBoardPieces/toSquaresArray';
 
 describe('accumulateIdxForMove Test Suite', () => {
-  it('accumulateIdxForMove#success', () => {
-    let pieces = createGameBoardPieces();
-    let squaresArr = toSquaresArray(pieces);
-    pieces.index = 29;
-    let indices = accumulateIdxForMove(pieces, squaresArr);
-    expect(indices.length).toEqual(2);
+  describe('initial board - index 29', () => {
+    let indices;
+    beforeAll( () => {
+      let pieces = createGameBoardPieces();
+      let squaresArr = toSquaresArray(pieces);
+      pieces.index = 29;
+      indices = accumulateIdxForMove(pieces, squaresArr);
+    });
+    it('indices.length=2', () => {
+      expect(indices.length).toEqual(2);
+    });
+    it('indices=[29,28]', () => {
+      expect(indices).toEqual([29,28]);
+    });
   });
+
 });
--- src/othello/controller/moveLogic/checkMove.test.js
+++ src/othello/controller/moveLogic/checkMove.test.js
@@ -5,30 +5,52 @@ import fs from 'fs';
 import path from 'path';
 
 describe('checkMove Test Suite', () => {
-  it('checkMove#success', (done) => {
-    const state = constructGame();
-    checkMove(state, 29)
-      .then(state => {
-        expect(state.history.length).toBe(2);
+  describe('initial board index 29', () => {
+    let game;
+    beforeAll( done => {
+      game = constructGame();
+      checkMove( game, 29 )
+      .then( state => {
+        expect( state.history.length).toEqual(2);
         done();
       })
-      .catch(err => { console.error( err ), done(err); });
+      .catch( err => done.fail(err) );
+    });
   });
 
-  it("checkMove#capture2", done => {
-    const file = fs.readFileSync(path.join(__dirname, "checkMove.test.capture2.json" ));
-    const gameState = createGame( JSON.parse(file) );
-    const debugBoard = gameState.history[ gameState.history.length - 1 ];
-    expect( debugBoard.player ).toEqual('B');
-    expect( debugBoard.validSquares[0] ).toEqual(0);
+  describe('capture2 - debug board', () => {
+    let debugBoard;
+    beforeAll( () => {
+      const file = fs.readFileSync(path.join(__dirname, "checkMove.test.capture2.json" ));
+      const gameState = createGame( JSON.parse(file) );
+      debugBoard = gameState.history[ gameState.history.length - 1 ];
+    });
+    it( 'debugBoard.player=B', () => {
+      expect(debugBoard.player).toEqual('B');
+    });
+    it( 'debugBoard.validSquares', () => {
+      expect( debugBoard.validSquares ).toEqual([0]);
+    })
+  });
 
-    checkMove( gameState, 0 )
+  describe('capture2 - debug board move index 0', () => {
+    let lastBoard;
+    beforeAll( ( done ) => {
+      const file = fs.readFileSync(path.join(__dirname, "checkMove.test.capture2.json" ));
+      const gameState = createGame( JSON.parse(file) );
+      checkMove( gameState, 0 )
       .then( state => {
-        const lastBoard = state.history[ state.history.length - 1];
-        expect( lastBoard.player ).toEqual('W');
+        lastBoard = state.history[ state.history.length - 1 ];
         done();
       })
-      .catch( err => { done.fail(err); });
+      .catch( err => done.fail( err ) );
+    });
 
+    it('lastBoard.player=W', () => {
+      expect( lastBoard.player ).toEqual( 'W' );
+    });
+    it('lastBoard.validSquares=[49,50,53]', () => {
+      expect( lastBoard.validSquares ).toEqual([49,50,53]);
+    });
   });
 });
