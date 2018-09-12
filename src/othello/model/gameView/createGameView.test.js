cf29762d79b5bf0e3e3eb78b10592872d4a1d70f
Arthur Anderson <arthur.anderson3@gmail.com>
2018-09-12 13:20:00 -0400

    ref atomic tests

--- src/othello/model/gameView/createGameView.test.js
+++ src/othello/model/gameView/createGameView.test.js
@@ -2,11 +2,21 @@ import createGameView from './createGameView';
 import createGameBoardPieces from '../gameBoardPieces/createGameBoardPieces';
 
 describe('createGameView test suite', () => {
-  it('#createGameView ', () => {
-    const { view } = createGameView( [createGameBoardPieces()] );
+  let view;
+  beforeAll( () => {
+    const game = createGameView( [createGameBoardPieces()] );
+    view = game.view;
+  });
+  it('view.currentPlayer', () => {
     expect(view).toHaveProperty('currentPlayer');
+  });
+  it('view.currentTurn', () => {
     expect(view).toHaveProperty('currentTurn');
+  });
+  it('view.lastTurnTime', () => {
     expect(view).toHaveProperty('lastTurnTime');
+  });
+  it('view.score', () => {
     expect(view).toHaveProperty('score');
   });
 });
