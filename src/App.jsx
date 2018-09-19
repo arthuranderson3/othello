d72db7f50b98a4489f615daf11427a32a66087ef
Arthur Anderson <arthur.anderson3@gmail.com>
2018-09-18 20:50:44 -0400

    reworked npm scripts for sass and material.io integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 import OthelloApp from './othello/OthelloApp';
 
@@ -7,9 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <CssBaseline>
-          <OthelloApp />
-        </CssBaseline>
+        <OthelloApp />
       </React.Fragment>
     );
   }
