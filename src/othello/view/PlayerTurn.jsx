0604a72b947a3b67217f32ac90599bea4f72e8aa
Arthur Anderson <arthur.anderson3@gmail.com>
2018-10-02 11:31:58 -0400

    testing the prettier task for scss files

--- src/othello/view/PlayerTurn.jsx
+++ src/othello/view/PlayerTurn.jsx
@@ -10,6 +10,15 @@ const PlayerTurn = ({ currentPlayer, currentTurn }) => (
     <h6>
       Turn: {currentTurn} Player: {GamePiece(currentPlayer)}
     </h6>
+    <svg
+      viewBox="0 0 100 100"
+      height="40px"
+      width="40px"
+      background="transparent"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="50" cy="50" r="40" fill="white" stroke="#888888" strokeWidth="2%" />
+    </svg>
   </div>
 );
 
