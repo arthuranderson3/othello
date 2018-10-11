c8b8964a5b73e34f5272a84f4941349bf692189f
Arthur Anderson <arthur.anderson3@gmail.com>
2018-10-11 11:14:51 -0400

    added check for production state

--- src/othello/view/ActionPanel.jsx
+++ src/othello/view/ActionPanel.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ActionButton from './controls/ActionButton';
 
-const ActionPanel = ({ title, onReset, onUndo, onDebugState }) => (
-  <div>
-    <h2>{title}</h2>
-    <ActionButton btnStyle="dark" onClick={onReset} text="Reset" />
-    &nbsp;
-    <ActionButton btnStyle="secondary" onClick={onUndo} text="Undo" />
-    &nbsp;
-    <ActionButton btnStyle="outline-danger" onClick={onDebugState} text="Debug Log" />
-  </div>
-);
-
+const ActionPanel = ({ title, onReset, onUndo, onDebugState }) => {
+  const devMode = process.env.NODE_ENV !== 'production';
+  if (devMode) {
+    return (
+      <div>
+        <h2>{title}</h2>
+        <ActionButton btnStyle="dark" onClick={onReset} text="Reset" />
+        &nbsp;
+        <ActionButton btnStyle="secondary" onClick={onUndo} text="Undo" />
+        &nbsp;
+        <ActionButton btnStyle="outline-danger" onClick={onDebugState} text="Debug Log" />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ActionButton btnStyle="dark" onClick={onReset} text="Reset" />
+      &nbsp;
+      <ActionButton btnStyle="secondary" onClick={onUndo} text="Undo" />
+    </div>
+  );
+};
 export default ActionPanel;
